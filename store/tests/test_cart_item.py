7395e6f3e23841898827e6e2e3e8e9fd1a68b827
Mojtaba Farzaneh <mojtabafarzaneh78@gmail.com>
2023-01-29 01:00:34 +0330

    finishing the cartitem tests

--- store/tests/test_cart_item.py
+++ store/tests/test_cart_item.py
@@ -23,10 +23,12 @@ def create_cart(api_client, authenticate_staff):
 def create_cart_item(api_client, authenticate_staff):
     def do_create_cart_item(cart, product):
         authenticate_staff(is_staff = True)
-        return api_client.post(f'/store/carts/{cart.id}/items/',{
+        response = api_client.post(f'/store/carts/{cart.id}/items/',{
             'product_id': product.id,
             'quantity': 3
         })
+        authenticate_staff(is_staff = False)
+        return response 
     return do_create_cart_item
 
 @pytest.mark.django_db
@@ -59,7 +61,6 @@ class TestCreateItem:
             'product_id': product.id,
             'quantity': 2})
         
-        print(response)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
         
     def test_data_invalid_return_400(self, api_client, authenticate_staff):
@@ -72,7 +73,6 @@ class TestCreateItem:
             'quantity': ''
         })
         
-        print(response.data)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['quantity'] is not None
     
@@ -95,12 +95,26 @@ class TestupdateItem:
     def test_data_valid_return_200(self, api_client, create_cart_item, authenticate_staff):
         product_id = baker.make(Product)
         cart_id = baker.make(Cart)
-        authenticate_staff(is_staff=False)
         create_item = create_cart_item(cart_id, product_id)
         data = create_item.data['id']
         response = api_client.patch(f'/store/carts/{cart_id.id}/items/{data}/',{'quantity': 4})
         
         assert response.status_code == status.HTTP_200_OK
+        assert response.data['quantity'] == 4
+        
+    def test_data_invalid_return_400(self, api_client, create_cart_item):
+        product_id = baker.make(Product)
+        cart_id = baker.make(Cart)
+        
+        create_item = create_cart_item(cart_id, product_id)
+        data = create_item.data['id']
+        
+        response = api_client.patch(f'/store/carts/{cart_id.id}/items/{data}/', {'quantity': ''})
+        
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['quantity'] is not None
+        
+        
     
     
 
