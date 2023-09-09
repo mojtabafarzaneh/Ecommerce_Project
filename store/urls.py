e3cc5d1245cfe7606110164cb917e1162f25c7e5
Mojtaba Farzaneh <you@example.com>
2023-09-09 19:47:40 +0330

    starting to make the Cart getway

--- store/urls.py
+++ store/urls.py
@@ -5,13 +5,17 @@ from rest_framework_nested import routers
 router = routers.SimpleRouter()
 router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
-
+router.register('carts', views.CartViewSet )
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup = 'cart')
+carts_router.register('items', views.CartItemsViewSet, basename = 'cart-items')
+
 
 urlpatterns = [
     path(r'', include(router.urls)),
-    path(r'', include(products_router.urls))
+    path(r'', include(products_router.urls)),
+    path(r'', include(carts_router.urls))
 ]
--- store/views.py
+++ store/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 
-from .models import Product, Collection, OrderItem, Review
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .models import Product, Collection, OrderItem, Review, Cart,CartItem
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer
 from .filters import ProductFilter
 
 
@@ -58,6 +59,24 @@ class ReviewViewSet(ModelViewSet):
     
     def get_serializer_context(self):
         return {'product_id' : self.kwargs['product_pk']}
+    
+
+class CartViewSet(CreateModelMixin,
+                  GenericViewSet, 
+                  RetrieveModelMixin, 
+                  DestroyModelMixin):
+    serializer_class = CartSerializer
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    
+class CartItemsViewSet(ModelViewSet):
+    def get_queryset(self, **kwargs):
+        return CartItem.objects.\
+            filter(cart_id=self.kwargs['cart_pk']).\
+            select_related('product')
+    
+    
+    serializer_class= CartItemSerializer
+    
 
     
     
